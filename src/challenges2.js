5e040215970cf1988eaa00cd50f96fb4b6175d86
João <santoscostaj808@gmail.com>
2021-12-03 13:04:51 -0300

    decimo requisito finalizado

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,15 @@
 // Desafio 10
-function techList() {
-  // seu código aqui
+function techList(array, nome) {
+  if (array.length !== 0) {
+    let tecs = [];
+    let sortarray = array.sort();
+    for (let i = 0; i < sortarray.length; i += 1) {
+      let objeto = { tech: sortarray[i], name: nome }
+      tecs.push(objeto)
+    }
+    return tecs;
+  }
+  return "Vazio!"
 }
 
 // Desafio 11
